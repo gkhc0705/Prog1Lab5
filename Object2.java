f1ea11d0ac2bdba9242adb388d33c9b80a45c16f
gkhc0705 <gkhc0119@gmail.com>
2020-04-26 23:04:49 -0400

    #55 of textbook questions part2

---
+++ Object2.java
@@ -0,0 +1,12 @@
+public class Object2
+{
+	public static void main(String[] args)
+	{
+		Encapsulation2 team = new Encapsulation2();
+
+		team.setConcept("Tiger");
+
+		System.out.println(team.toString());
+	}
+}
+
