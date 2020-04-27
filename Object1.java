23bf55edfd4d5908c52da2c079cd0beeeb8c34fd
gkhc0705 <gkhc0119@gmail.com>
2020-04-26 22:55:00 -0400

    #57 of textbook question part2

---
+++ Object1.java
@@ -0,0 +1,13 @@
+public class Object1
+{
+	public static void main(String[] args)
+	{
+		Encapsulation table = new Encapsulation();
+		table.setName("Chanki");
+		table.setGrades(95);
+		System.out.println(table.toString());
+
+
+
+	}
+}
