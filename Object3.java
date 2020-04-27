d5b9337a19f004eaa231c6400b0a94adf0169457
gkhc0705 <gkhc0119@gmail.com>
2020-04-26 23:14:18 -0400

    #56 of textbook questions part2

---
+++ Object3.java
@@ -0,0 +1,12 @@
+public class Object3
+{
+	public static void main(String[] args)
+	{
+		Encapsulation3 television = new Encapsulation3();
+
+		television.setBrand("Samsung");
+		television.setPrice(1000);
+
+		System.out.println(television.toString());
+	}
+}
